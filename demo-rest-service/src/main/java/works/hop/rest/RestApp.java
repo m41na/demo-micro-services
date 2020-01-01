1e94e09574d9a88a704ebefd3b0bb2edd1a56473
smaina <a-smaina@expedia.com>
2019-12-31 18:54:38 -0600

    added resilience decorators to the http client and cleaned up a bunch of useless artifacts

--- demo-rest-service/src/main/java/works/hop/rest/RestApp.java
+++ demo-rest-service/src/main/java/works/hop/rest/RestApp.java
@@ -2,9 +2,11 @@ package works.hop.rest;
 
 import com.practicaldime.zesty.app.AppProvider;
 import com.practicaldime.zesty.app.AppServer;
+import com.practicaldime.zesty.servlet.HandlerException;
 import org.eclipse.jetty.http.HttpMethod;
 import works.hop.web.HttpConnect;
 
+import java.io.IOException;
 import java.util.Collections;
 
 public class RestApp {
@@ -18,7 +20,12 @@ public class RestApp {
                             res.send("rest: " + new String(response.body));
                         });
             } catch (Exception e) {
-                e.printStackTrace();
+                try {
+                    res.sendError(500, e.getMessage());
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                    throw new HandlerException(500, "Problem accessing search", ex);
+                }
             }
             done.complete();
         });
